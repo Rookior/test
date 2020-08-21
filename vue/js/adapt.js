e745be6c51aca3a11c9b1f6c4b3d6d70a92f675d
= <1757629766@qq.com>
2020-08-21 16:33:01 +0800

    vue基础

---
+++ vue/js/adapt.js
@@ -0,0 +1,15 @@
+
+function setRootFontSize(){
+    var doc = document;
+    var docEle = doc.documentElement;
+    var nw = docEle.getBoundingClientRect().width;
+    var w = 1920;
+    //w->nw ==> w * nw/w = nw;
+    if(nw<w){
+       scale = nw/w;
+    }else{
+        scale = nw/w;
+    }
+   doc.querySelector('#main').style = 'transform: scale('+scale+')';     
+}
+setRootFontSize()
