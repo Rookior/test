18a118f5751d5c2cae9e90d352af30534ccc69a4
rookior <1757629766@qq.com>
2020-10-12 15:16:08 +0800

    对象析构与babel语法转化

---
+++ ES6/babel-es6/src/index.js
@@ -0,0 +1,10 @@
+const PI = 3.14
+const sum = (x,y)=>{
+    return x+y
+}
+const {name,age}={
+    name:"张三2",
+    age:30
+}
+
+// 通过执行npx babel src/index -o dist/index.js 将当前js编译为es5语法到dist文件夹
---
+++ ES6/babel-es6/src/index2.js
@@ -0,0 +1,6 @@
+const a =1;
+const b =2
+
+//通过 npx babel src -d dist 将src里面的文件都编译出来
+
+//通过 npx babel src -w -d dist 实时监听文件修改保存后，自动更新到dist
